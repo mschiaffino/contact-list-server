209b38ec5cd14086d8b2fa7791fd49f7ed68f6b7
Martín Schiaffino <schiaffinomartin@gmail.com>
2017-07-17 20:38:19 -0300

    Generated contacts service

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,5 @@
+const contacts = require('./contacts/contacts.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
+  app.configure(contacts);
 };
